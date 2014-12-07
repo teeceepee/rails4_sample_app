1cb64a666bd86e577aac95103404b65e8c8d496f
Yu Haidong <yuhaidongyhd@gmail.com>
2014-12-07 21:23:01 +0800

    Add user microposts.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def new
@@ -33,6 +34,7 @@ class UsersController < ApplicationController
       flash[:error] = "User cannot destroy himself!"
       redirect_to user_path(current_user)
     else
+      user.destroy
       flash[:success] = "User destroyed."
       redirect_to users_url
     end
@@ -58,13 +60,6 @@ class UsersController < ApplicationController
   end
 
   # Before filters
-
-  def signed_in_user
-    unless signed_in?
-      store_location
-      redirect_to signin_url, notice: "Please sign in."
-    end
-  end
 
   def correct_user
     @user = User.find(params[:id])
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  has_many :microposts, dependent: :destroy
+
   before_save { self.email.downcase! }
   before_create :create_remember_token
 
@@ -11,6 +13,11 @@ class User < ActiveRecord::Base
 
   has_secure_password
   validates :password, length: { minimum: 6 }
+
+  def feed
+    # This is preliminary. See "Following users" for the full implementation.
+    Micropost.where("user_id = ?", id)
+  end
 
   def self.new_remember_token
     SecureRandom.urlsafe_base64
